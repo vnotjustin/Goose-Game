bdd144268684cd91a90d0a344d34ec9ece7a4f59
Zittacheng <zittacheng@gmail.com>
2019-11-02 16:38:33 -0400

    Pathfinding

---
+++ Assets/AIStuff/Script/Editor/Editor_PathObstacle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Z
+{
+    [CustomEditor(typeof(PathObstacle))]
+    [CanEditMultipleObjects]
+    public class Editor_PathObstacle : Editor {
+
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+            PathObstacle PO = (PathObstacle)target;
+            if (GUILayout.Button("Apply"))
+            {
+                Undo.RegisterFullObjectHierarchyUndo(PO.gameObject, "FindPoints");
+                PO.EditorApply();
+            }
+        }
+    }
+}
