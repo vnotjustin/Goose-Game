5bcc8452cef3cd0f6361e5c74a17e613b95048a8
Justin <justintrongvo@gmail.com>
2019-11-20 12:12:48 -0500

    changes

--- Assets/Goose Scripts/gooseControl.cs
+++ Assets/Goose Scripts/gooseControl.cs
@@ -12,7 +12,7 @@ public class gooseControl : MonoBehaviour
     private float defaultY = .5f;
     public float movespeed;
     public GameObject neck;
-
+    Animator m_Animator;
     private Vector3 targetPos;
     public Transform target;
     public LayerMask ground;
@@ -24,7 +24,7 @@ public class gooseControl : MonoBehaviour
         targetPos = transform.position;
         Cursor.visible = true;
         Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
-        
+        m_Animator = GetComponentInChildren<Animator>();
         //neckD.rotation = Quaternion.Euler(110, 0, 0);
         //neckU.rotation = Quaternion.Euler(30, 0, 0);
     }
@@ -32,14 +32,16 @@ public class gooseControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
         float rcDist = 1000f;
         Debug.DrawRay(mouseRay.origin, mouseRay.direction * rcDist, Color.red);
         RaycastHit mouseHit = new RaycastHit();
 
+
         if (Input.GetMouseButton(0))
         {
-            if (Physics.Raycast(mouseRay, out mouseHit, rcDist, ground))
+                if (Physics.Raycast(mouseRay, out mouseHit, rcDist, ground))
             {
                 targetPos = mouseHit.point;
                 targetPos.y = defaultY;
@@ -52,18 +54,26 @@ public class gooseControl : MonoBehaviour
             if ((Vector3)transform.position != targetPos)
             {
                 transform.position = Vector3.MoveTowards(transform.position, targetPos, movespeed  * Time.deltaTime);
+                m_Animator.SetBool("isWalking", true);
             }
 
         }
+        else
+        {
+            m_Animator.SetBool("isWalking", false);
+        }
+
 
 
         if (Input.GetKey(KeyCode.LeftShift))
         {
             movespeed = 7;
+            m_Animator.SetBool("isSprint", true);
         }
         else
         {
             movespeed = 4;
+            m_Animator.SetBool("isSprint", false);
         }
 
 
