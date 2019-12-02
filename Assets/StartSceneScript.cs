3e19caf9d4b1e223f4926d6def2fb081875c9a7a
lynntrassh <lynntrassh@gmail.com>
2019-12-02 04:38:40 -0500

    Added  3d asset (Hat), and start scene.

---
+++ Assets/StartSceneScript.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartSceneScript : MonoBehaviour
+{
+    public Button[] buttons;
+    public GameObject[] dots;
+    public Text[] texts;
+    public bool button0; public bool button1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (button0)
+        {
+            buttons[0].onClick.AddListener(StartGame);
+        }
+
+        if(button1)
+        {
+            buttons[1].onClick.AddListener(QuitGame);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void StartGame()
+    {
+        Debug.Log("Game Starts");
+        SceneManager.LoadScene("GooseScene");
+
+    }
+    void QuitGame()
+    {
+        Debug.Log("Game Quits");
+        Application.Quit();
+    }
+
+    public void OnMouseOver()
+    {
+        if(button0)
+        {
+            dots[0].GetComponent<SpriteRenderer>().enabled = true;
+           
+           
+            
+        }
+
+        if(button1)
+        {
+            dots[1].GetComponent<SpriteRenderer>().enabled = true;
+        }
+    }
+
+    public void OnMouseExit()
+    {
+        if (button0)
+        {
+            
+            
+            dots[0].GetComponent<SpriteRenderer>().enabled = false;
+            
+        }
+
+        if (button1)
+        {
+            dots[1].GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+}
